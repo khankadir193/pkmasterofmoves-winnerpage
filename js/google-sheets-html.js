6b10bedf8bea4883b04981c12d678ad2940fec75
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-26 16:18:43 +0530

    pkWinnerMoves

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -123,26 +123,26 @@ $('#conDivUser1').append('<div class="colThree"><div class="imgClass1"><a href="
 
     $('#conDivUser3').append('<div class="colThree"><div class="imgClass3"><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
       }
-  else  if(counter>2 && counter<=5){
+  else  if(counter>2 && counter<=6){
 
     $('#conDivUser').append('<div class="colThree1"><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass4"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
       }
 
   
 
-  else  if(counter==6){
+  else  if(counter==7){
 
     $('#conDivTalent1').append('<div class="colThree"><div class="imgClass1"><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
       }
-  else  if(counter==7){
+  else  if(counter==8){
 
     $('#conDivTalent2').append('<div class="colThree"><div class="imgClass2"><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
       }
-  else  if(counter==8){
+  else  if(counter==9){
 
     $('#conDivTalent3').append('<div class="colThree"><div class="imgClass3"><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
       }
- else  if(counter>=9 && counter<=9){
+ else  if(counter>=10 && counter<=11){
 
     $('#conDivTalent4').append('<div class="colThree1"><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass4"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
       }
